c2b52df28c5326de53bb199aeefd8f9313430dca
Tima1278 <104497043+Tima1278@users.noreply.github.com>
2024-05-22 13:37:51 +0000

    deleted all

--- meteo/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import VueApexCharts from 'vue3-apexcharts';
-
-const app = createApp(App);
-
-app.component('ApexChartComponent', VueApexCharts);
-
-app.mount('#app');
